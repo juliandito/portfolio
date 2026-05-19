87977ce506b82ca2cd506db009117d3e479d6c19
Kevin Juliandito Suhartono <vjuliandito@gmail.com>
2026-05-19 18:09:39 +0700

    Added gh page package and config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/portfolio/',
+export default defineConfig(() => ({
+  base: '/',
   plugins: [react(), tailwindcss()],
 }))
